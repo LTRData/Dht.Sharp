7149ad6017aa7173f18ba5ca005f4c518df42c46
Olof Lagerkvist <ol@ltr-data.se>
2022-11-16 12:29:21 +0100

    .NET 7 migrations etc

--- Dht.Sharp/Properties/AssemblyInfo.cs
+++ Dht.Sharp/Properties/AssemblyInfo.cs
@@ -4,14 +4,6 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Dht.Sharp")]
-[assembly: AssemblyDescription("Native C# library for the DHT11 and DHT22 sensors.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Daniel M. Porrey, Olof Lagerkvist")]
-[assembly: AssemblyProduct("Dht.Sharp")]
-[assembly: AssemblyCopyright("Copyright © 2018-2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -23,6 +15,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
 [assembly: ComVisible(false)]
